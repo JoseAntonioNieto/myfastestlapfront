9f3e6fd9f2385bae5531d101e267f7e862e1dde0
Jose Antonio <gokugamer243@gmail.com>
2023-04-03 11:25:05 +0200

    feat: Obtencion info usuario

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { faS } from '@fortawesome/free-solid-svg-icons';
 import {GoogleApiService} from "../services/google-api.service";
+import { UserDataService } from '../services/menu/user-data.service';
 
 @Component({
   selector: 'app-menu',
@@ -14,19 +15,34 @@ export class MenuComponent {
 
   userInfo?: any;
 
-  constructor(public readonly google: GoogleApiService) {
+  constructor(public readonly google: GoogleApiService, private user_data: UserDataService) {
     google.userProfileSubject.subscribe(info => {
       this.userInfo = info;
     })
+  }
+
+  getUserInfo(): void {
+    this.user_data.getUserData(this.google.getToken()).subscribe(
+      (data) => {
+        this.userInfo = data;
+      }
+    )
+  }
+
+  obtenerImagen(): String {
+    this.getUserInfo();
+    return this.userInfo.picture;
+  }
+
+  obtenerNombre(): String {
+    this.getUserInfo();
+    return this.userInfo.name;
   }
 
   isLoggedIn(): boolean {
     return this.google.isLoggedIn();
   }
 
-  getToken(): string {
-    return this.google.getToken();
-  }
 
   logout() {
     this.google.signOut();
---
+++ src/app/services/menu/user-data.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserDataService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getUserData(token: any): Observable<any> {
+    const headers = new HttpHeaders().set("authentication", token);
+    return this.httpClient.get<any>("http://localhost:5000/api/user_data", {"headers": headers});
+  }
+}
