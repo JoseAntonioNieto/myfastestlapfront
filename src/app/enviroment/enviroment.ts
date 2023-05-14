50d1f2f569d962e68d96e3fa7d37d145766fcc4a
Jose Antonio <gokugamer243@gmail.com>
2023-05-14 19:02:50 +0200

    feat: Enviroment

--- src/app/enviroment/enviroment.ts
+++ src/app/enviroment/enviroment.ts
@@ -1,4 +1,5 @@
 export const enviroment = {
   google_client_id: "626104102087-raim7uq02tefr5djn5sppq38ka3ksihr.apps.googleusercontent.com",
+  // api_url: "http://localhost:5000/"
   api_url: "https://myfastestlap.herokuapp.com/"
 }
